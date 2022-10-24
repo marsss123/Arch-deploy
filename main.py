7f7d00b4a10ffd229eef5a3d321a7dc73079ec43
spyrosmaris <smar@pop-os.localdomain>
2022-10-24 17:41:48 +0300

    On branch main  Changes to be committed: 	deleted:    README.md 	deleted:    config 	modified:   main.py 	deleted:    setup.sh

--- main.py
+++ main.py
@@ -1,136 +1,222 @@
-import subprocess
 import os
-import sys
-from subprocess import call
+import sys   
 
-class insdisk:
-    def __init__(self,bootselect,rootselect,swapselect):
-        self.bootselect = bootselect
-        self.rootselect = rootselect
-        self.swapselect = swapselect
-        
-    def root(self):
-        os.system(["mkfs.ext4", self.rootselect])
-        os.system(["mount", self.rootselect]) 
-        os.system(['mount', self.rootselect, '/mnt'])
+def archinstalldisk():
+    # Get the disk to install to
+    print(os.system("lsblk"))
+    print("Please enter the disk you want to install to")
+    getdisk=input()
+    print("You have selected", getdisk)
+    print("Is this correct? (y/n)")
+    getdiskconfirm=input()
+    if getdiskconfirm == "y":
+        print("Continuing...")
+    else:  
+        print("Please restart the installer")
+        exit()
+    print("making partitions")
+    print("do you want a swap space")
+    getswap=input()
+    if getswap == "y":
+        print("making swap")
+        os.system("mkswap /dev/" + getdisk + "2")
+        os.system("swapon /dev/" + getdisk + "2")
+    else:
+        print("continuing")
+    print("making root and boot partitions")
 
-    def boot(self):
-        os.system(["mkfs.fat -F32", self.bootselect])
-        os.system(["mount", self.bootselect])
-        os.system(["mkdir", "/mnt/boot"])
-        os.system(["mount", self.bootselect, "/mnt/boot"])
+    os.system("parted -s /dev/" + getdisk + " mklabel gpt")
+    os.system("parted -s /dev/" + getdisk + " mkpart primary fat32 1MiB 513MiB")
+    os.system("parted -s /dev/" + getdisk + " set 1 esp on")
+    print("please seleft file system for root partition")
+    print("1.ext4")
+    print("2.btrfs")
+    print("3.xfs")
+    print("4.f2fs")
+    print("5.jfs")
+    print("6.reiserfs")
+    print("7.ntfs")
+    print("8.exfat")
+    print("9.fat32")
+    getrootfs=input()
+    if getrootfs == "1":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary ext4 513MiB 100%")
+    elif getrootfs == "2":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary btrfs 513MiB 100%")
+    elif getrootfs == "3":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary xfs 513MiB 100%")
+    elif getrootfs == "4":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary f2fs 513MiB 100%")
+    elif getrootfs == "5":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary jfs 513MiB 100%")
+    elif getrootfs == "6":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary reiserfs 513MiB 100%")
+    elif getrootfs == "7":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary ntfs 513MiB 100%")
+    elif getrootfs == "8":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary exfat 513MiB 100%")
+    elif getrootfs == "9":
+        os.system("parted -s /dev/" + getdisk + " mkpart primary fat32 513MiB 100%")
+    else:
+        print("invalid input")
+        exit()
+    print("formatting partitions")
+    os.system("mkfs.fat -F32 /dev/" + getdisk + "1")
+    if getrootfs == "1":
+        os.system("mkfs.ext4 /dev/" + getdisk + "2")
+    elif getrootfs == "2":
+        os.system("mkfs.btrfs /dev/" + getdisk + "2")
+    elif getrootfs == "3":
+        os.system("mkfs.xfs /dev/" + getdisk + "2")
+    elif getrootfs == "4":
+        os.system("mkfs.f2fs /dev/" + getdisk + "2")
+    elif getrootfs == "5":
+        os.system("mkfs.jfs /dev/" + getdisk + "2")
+    elif getrootfs == "6":
+        os.system("mkfs.reiserfs /dev/" + getdisk + "2")
+    elif getrootfs == "7":
+        os.system("mkfs.ntfs /dev/" + getdisk + "2")
+    elif getrootfs == "8":
+        os.system("mkfs.exfat /dev/" + getdisk + "2")
+    elif getrootfs == "9":
+        os.system("mkfs.fat -F32 /dev/" + getdisk + "2")
+    else:
+        print("invalid input")
+        exit()
+    print("mounting partitions")
+    os.system("mount /dev/" + getdisk + "2 /mnt")
+    os.system("mkdir /mnt/boot")
+    os.system("mount /dev/" + getdisk + "1 /mnt/boot")
+    print("installing base system")
+    os.system('pacstrap', '/mnt', 'base' , 'linux', 'linux-firmware' , 'linux-headers','base-devel','vim','networkmanager')
+    print("generating fstab")
+    os.system("genfstab -U /mnt >> /mnt/etc/fstab")
+    print("chrooting into new system")
+    os.system("arch-chroot /mnt")
+    print("setting up time")
+    os.system("ln -sf /usr/share/zoneinfo/Europe/Athens /etc/localtime")
+    print("setting up new system")
+    print("setting up locale")
+    os.system("echo LANG=en_US.UTF-8 > /etc/locale.conf")
+    os.system("echo en_US.UTF-8 UTF-8 > /etc/locale.gen")
+    os.system("echo en_US ISO-8859-1 >> /etc/locale.gen")
+    os.system("locale-gen")
+    print("setting up hostname")
+    gethostname=input()
+    os.system("echo " + gethostname + " > /etc/hostname")
+    print("setting up initramfs")
+    os.system("mkinitcpio -P")
+    print("setting up root password")
+    get_password=input()
+    os.system("echo root:" + get_password + " | chpasswd")
+    print ("do you want to add a user?")
+    getuser=input()
+    if getuser == "y":
+        print("enter username")
+        getusername=input()
+        os.system("useradd -m -g users -G wheel -s /bin/bash " + getusername)
+        print("setting up user password")
+        get_password2=input()
+        os.system("echo " + getusername + ":" + get_password2 + " | chpasswd")
+        print("adding user to sudoers")
+        os.system("echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers")
+    else:
+        print("continuing")
+    print("setting up bootloader")
+    os.system("pacman -S grub efibootmgr os-prober")
+    os.system("grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB")
+    os.system("grub-mkconfig -o /boot/grub/grub.cfg")
+    print("setting up network")
+    os.system("systemctl enable NetworkManager")
+    print("do you want to continue with extra post install script?")
+    getpost=input()
+    if getpost == "y":
+        print("continuing")   
+    else:
+        print("exiting")
+        exit()
+    os.system("su " + getusername)
+    print("do you want to install a desktop environment?")
+    getdesktop=input()
+    if getdesktop == "y":
+        print("please select a desktop environment")
+        print("1.kde")
+        print("2.gnome")
+        print("3.xfce")
+        print("4.i3-gaps")
+        getdesktop=input()
+        if getdesktop == "1":
+            os.system("pacman -S plasma-meta")
+        elif getdesktop == "2":
+            os.system("pacman -S gnome")
+        elif getdesktop == "3":
+            os.system("pacman -S xfce4")
+        elif getdesktop == "4":
+            print("flagship")
+            os.system("pacman -S i3-gaps")
+    else:
+        print("ok")
+    print("do you want to install a display manager?")
+    getdm=input()
+    if getdm == "y":
+        print("please select a display manager")
+        print("1.sddm")
+        print("2.gdm")
+        print("3.lightdm")
+        getdm=input()
+        if getdm == "1":
+            os.system("pacman -S sddm")
+            os.system("systemctl enable sddm")
+        elif getdm == "2":
+            os.system("pacman -S gdm")
+            os.system("systemctl enable gdm")
+        elif getdm == "3":
+            os.system("pacman -S lightdm")
+            os.system("systemctl enable lightdm")
+    else:
+        print("ok")
+    print("do you want to install a aur helper?")
+    getaur=input()
+    if getaur == "y":
+        print("please select a aur helper")
+        print("1.yay")
+        print("2.pikaur")
+        print("3.paru")
+        getaur=input()
+        if getaur == "1":
+            os.system("pacman -S git")
+            os.system("git clone https://aur.archlinux.org/yay.git")
+            os.system("cd yay")
+            os.system("makepkg -si")
+        elif getaur == "2":
+            os.system("pacman -S git")
+            os.system("git clone https://aur.archlinux.org/pikaur.git")
+            os.system("cd pikaur")
+            os.system("makepkg -si")
+        elif getaur == "3":
+            os.system("pacman -S git")
+            os.system("git clone https://aur.archlinux.org/paru.git")
+            os.system("cd paru")
+            os.system("makepkg -si")
+    else:
+        print("ok") 
 
-    def swap(self):
-        os.system(["mkswap", self.swapselect])
-        os.system(["swapon", self.swapselect])
+    print("do you want to install a browser?")
+    getbrowser=input()
+    if getbrowser == "y":
+        print("please select a browser")
+        print("1.firefox")
+        print("2.google chrome")
+        getbrowser=input()
+        if getbrowser == "1":
+            os.system("pacman -S firefox")
+        elif getbrowser == "2":
+            os.system(getaur + "-S google-chrome")
+    else:
+        print("ok")
+    print("tank you for using my script")
+    print("exiting")
+    exit()
 
-class install:
-    def __init__(self):
-        self.install()
-        self.postchroot()
-    def install(self):
-        os.system(['pacstrap', '/mnt', 'base' , 'linux', 'linux-firmware' , 'linux-headers','base-devel','vim','networkmanager'])
-        os.system('genfstab', '-U', '/mnt', '>>', '/mnt/etc/fstab')
-        os.system('arch-chroot', '/mnt')
-    def postchroot(self):
-        os.system('arch-chroot', '/mnt')
-        os.system('ln -sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
-        os.system('hwclock', '--systohc')
-        os.system('vim', '/etc/locale.gen')
-        os.system('locale-gen')
-        os.system('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
-        os.system('echo', 'KEYMAP=us', '>>', '/etc/vconsole.conf')
-        os.system('echo', 'ArchBTW', '>>', '/etc/hostname')
+    
 
-class postchroot:
-    def __init__(self,settime,hwclock,locale,localegen,localeconf,hostname,rootpass,bootloader,grubinstall,grubmkconfig,useradd,userpass,sudoers,network,wmdeployscript):
-        self.settime = settime()
-        self.hwclock = hwclock()
-        self.locale = locale()
-        self.localegen = localegen()
-        self.localeconf = localeconf()
-        self.hostname = hostname()
-        self.rootpass = rootpass()
-        self.bootloader = bootloader()
-        self.grubinstall = grubinstall()
-        self.grubmkconfig = grubmkconfig()
-        self.useradd = useradd()
-        self.userpass = userpass()
-        self.sudoers = sudoers()
-        self.network = network()
-        self.wmdeployscript = wmdeployscript()
-        self.exit = exit()
-        self.umount = umount()
-        self.reboot = reboot()
-    def settime(self):
-        os.system('ln', '-sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
-    def hwclock(self):
-        os.system('hwclock', '--systohc')
-    def locale(self):
-        os.system('echo', 'en_US.UTF-8 UTF-8', '>>', '/etc/locale.gen')
-    def localegen(self):
-        os.system('locale-gen')
-    def localeconf(self):
-        os.system('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
-    def hostname(self):
-        os.system('echo', 'archlinux', '>>', '/etc/hostname')
-    def rootpass(self):
-        os.system('passwd')
-    def bootloader(self):
-        os.system('pacman', '-S','grub','efibootmgr','os-prober')
-    def grubinstall(self):
-        os.system('grub-install', '--target=x86_64-efi', '--efi-directory=/boot', '--bootloader-id=GRUB')
-    def grubmkconfig(self):
-        os.system('grub-mkconfig', '-o', '/boot/grub/grub.cfg')
-    def useradd(self):
-        os.system('useradd', '-m', '-G', 'wheel', 'ArchBtw')
-    def userpass(self):
-        os.system('passwd', 'ArchBTW')
-    def sudoers(self):
-        os.system('echo', '%wheel ALL=(ALL) ALL', '>>', '/etc/sudoers')
-    def network(self):
-        os.system('systemctl', 'enable', 'NetworkManager')
-    def wmdeployscript(self):
-        os.system('chmode +x', '~/Arch-deploy/setup.sh')
-        os.system('sh', '~/Arch-deploy/setup.sh' )    
-    def exit(self):
-        os.system('exit')
-    def umount(self):
-        os.system('umount', '-R', '/mnt')
-    def reboot(self):
-        os.system('reboot')  
-
-class main:
-    def __init__(self,insdisk,install,postchroot):
-        self.insdisk = insdisk()
-        self.install = install()
-        self.postchroot = postchroot()
-    def insdisk(self):
-        self.insdisk.format()
-        self.insdisk.mount()
-    def install(self):
-        self.install.install()
-    def postchroot(self):
-        self.postchroot.settime()
-        self.postchroot.hwclock()
-        self.postchroot.locale()
-        self.postchroot.localegen()
-        self.postchroot.localeconf()
-        self.postchroot.hostname()
-        self.postchroot.rootpass()
-        self.postchroot.bootloader()
-        self.postchroot.grubinstall()
-        self.postchroot.grubmkconfig()
-        self.postchroot.useradd()
-        self.postchroot.userpass()
-        self.postchroot.sudoers()
-        self.postchroot.network()
-        self.postchroot.wmdeployscript()
-        self.postchroot.exit()
-        self.postchroot.umount()
-        self.postchroot.reboot()
-
-if __name__ == '__main__':
-    a = insdisk(bootselect=input("Enter the boot partition path: "),rootselect=input("Enter the root partition path: "),swapselect=input("Enter the swap partition path: "))
-    install()
-    postchroot()
