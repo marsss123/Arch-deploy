26cdd429e0b9f3a05884a07106960c5aa1faec93
spyrosmaris <smar@pop-os.localdomain>
2022-10-24 11:54:14 +0300

    On branch main  Changes to be committed: 	deleted:    ArchPost 	modified:   main.py

--- main.py
+++ main.py
@@ -4,50 +4,43 @@ import sys
 from subprocess import call
 
 class insdisk:
-    bootselect = input("Enter the boot partition path: ")
-    rootselect = input("Enter the root partition path: ")
-    swapselect = input("Enter the swap partition path: ")
-    def __init__(self):
-        self.bootselect = insdisk.bootselect
-        self.rootselect = insdisk.rootselect
-        self.swapselect = insdisk.swapselect
-    def boot(self):
-        subprocess.call(["mkfs.fat -F32", self.bootselect])
-        subprocess.call(["mount", self.bootselect, "/mnt/boot"])
+    def __init__(self,bootselect,rootselect,swapselect):
+        self.bootselect = bootselect
+        self.rootselect = rootselect
+        self.swapselect = swapselect
+        
     def root(self):
-        subprocess.call(["mkfs.ext4", self.rootselect])
-        subprocess.call(["mount", self.rootselect, "/mnt"]) 
+        os.system(["mkfs.ext4", self.rootselect])
+        os.system(["mount", self.rootselect]) 
+        os.system(['mount', self.rootselect, '/mnt'])
+
+    def boot(self):
+        os.system(["mkfs.fat -F32", self.bootselect])
+        os.system(["mount", self.bootselect])
+        os.system(["mkdir", "/mnt/boot"])
+        os.system(["mount", self.bootselect, "/mnt/boot"])
+
     def swap(self):
-        subprocess.call(["mkswap", self.swapselect])
-        subprocess.call(["swapon", self.swapselect])
+        os.system(["mkswap", self.swapselect])
+        os.system(["swapon", self.swapselect])
 
 class install:
-    def __init__(self,format,mount,install,postchroot):
-        self.format = format()
-        self.mount = mount()
-        self.install = install()
-        self.postchroot = postchroot()
-    def format(self):
-        self.format.boot()
-        self.format.root()
-        self.format.swap()
-    def mount(self):
-        self.mount.boot()
-        self.mount.root()
-        self.mount.swap()
+    def __init__(self):
+        self.install()
+        self.postchroot()
     def install(self):
-        subprocess.call(['pacstrap', '/mnt', 'base' , 'linux', 'linux-firmware' , 'linux-headers','base-devel','vim','networkmanager'])
-        subprocess.call('genfstab', '-U', '/mnt', '>>', '/mnt/etc/fstab')
-        subprocess.call('arch-chroot', '/mnt')
+        os.system(['pacstrap', '/mnt', 'base' , 'linux', 'linux-firmware' , 'linux-headers','base-devel','vim','networkmanager'])
+        os.system('genfstab', '-U', '/mnt', '>>', '/mnt/etc/fstab')
+        os.system('arch-chroot', '/mnt')
     def postchroot(self):
-        subprocess.call('arch-chroot', '/mnt')
-        subprocess.call('ln -sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
-        subprocess.call('hwclock', '--systohc')
-        subprocess.call('vim', '/etc/locale.gen')
-        subprocess.call('locale-gen')
-        subprocess.call('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
-        subprocess.call('echo', 'KEYMAP=us', '>>', '/etc/vconsole.conf')
-        subprocess.call('echo', 'ArchBTW', '>>', '/etc/hostname')
+        os.system('arch-chroot', '/mnt')
+        os.system('ln -sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
+        os.system('hwclock', '--systohc')
+        os.system('vim', '/etc/locale.gen')
+        os.system('locale-gen')
+        os.system('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
+        os.system('echo', 'KEYMAP=us', '>>', '/etc/vconsole.conf')
+        os.system('echo', 'ArchBTW', '>>', '/etc/hostname')
 
 class postchroot:
     def __init__(self,settime,hwclock,locale,localegen,localeconf,hostname,rootpass,bootloader,grubinstall,grubmkconfig,useradd,userpass,sudoers,network,wmdeployscript):
@@ -70,42 +63,42 @@ class postchroot:
         self.umount = umount()
         self.reboot = reboot()
     def settime(self):
-        subprocess.call('ln', '-sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
+        os.system('ln', '-sf', '/usr/share/zoneinfo/Asia/Kolkata', '/etc/localtime')
     def hwclock(self):
-        subprocess.call('hwclock', '--systohc')
+        os.system('hwclock', '--systohc')
     def locale(self):
-        subprocess.call('echo', 'en_US.UTF-8 UTF-8', '>>', '/etc/locale.gen')
+        os.system('echo', 'en_US.UTF-8 UTF-8', '>>', '/etc/locale.gen')
     def localegen(self):
-        subprocess.call('locale-gen')
+        os.system('locale-gen')
     def localeconf(self):
-        subprocess.call('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
+        os.system('echo', 'LANG=en_US.UTF-8', '>>', '/etc/locale.conf')
     def hostname(self):
-        subprocess.call('echo', 'archlinux', '>>', '/etc/hostname')
+        os.system('echo', 'archlinux', '>>', '/etc/hostname')
     def rootpass(self):
-        subprocess.call('passwd')
+        os.system('passwd')
     def bootloader(self):
-        subprocess.call('pacman', '-S','grub','efibootmgr','os-prober')
+        os.system('pacman', '-S','grub','efibootmgr','os-prober')
     def grubinstall(self):
-        subprocess.call('grub-install', '--target=x86_64-efi', '--efi-directory=/boot', '--bootloader-id=GRUB')
+        os.system('grub-install', '--target=x86_64-efi', '--efi-directory=/boot', '--bootloader-id=GRUB')
     def grubmkconfig(self):
-        subprocess.call('grub-mkconfig', '-o', '/boot/grub/grub.cfg')
+        os.system('grub-mkconfig', '-o', '/boot/grub/grub.cfg')
     def useradd(self):
-        subprocess.call('useradd', '-m', '-G', 'wheel', 'ArchBtw')
+        os.system('useradd', '-m', '-G', 'wheel', 'ArchBtw')
     def userpass(self):
-        subprocess.call('passwd', 'ArchBTW')
+        os.system('passwd', 'ArchBTW')
     def sudoers(self):
-        subprocess.call('echo', '%wheel ALL=(ALL) ALL', '>>', '/etc/sudoers')
+        os.system('echo', '%wheel ALL=(ALL) ALL', '>>', '/etc/sudoers')
     def network(self):
-        subprocess.call('systemctl', 'enable', 'NetworkManager')
+        os.system('systemctl', 'enable', 'NetworkManager')
     def wmdeployscript(self):
-        subprocess.call('chmode +x', '~/Arch-deploy/ArchPost/setup.sh')
-        subprocess.call('sh', '~/Arch-deploy/ArchPost/setup.sh' )    
+        os.system('chmode +x', '~/Arch-deploy/setup.sh')
+        os.system('sh', '~/Arch-deploy/setup.sh' )    
     def exit(self):
-        subprocess.call('exit')
+        os.system('exit')
     def umount(self):
-        subprocess.call('umount', '-R', '/mnt')
+        os.system('umount', '-R', '/mnt')
     def reboot(self):
-        subprocess.call('reboot')  
+        os.system('reboot')  
 
 class main:
     def __init__(self,insdisk,install,postchroot):
@@ -136,3 +129,8 @@ class main:
         self.postchroot.exit()
         self.postchroot.umount()
         self.postchroot.reboot()
+
+if __name__ == '__main__':
+    a = insdisk(bootselect=input("Enter the boot partition path: "),rootselect=input("Enter the root partition path: "),swapselect=input("Enter the swap partition path: "))
+    install()
+    postchroot()
