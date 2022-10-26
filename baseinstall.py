b2c25c0526cf550a99cbba1d8d8ac0fc692b2ba3
marsss123 <spyrosmaris@gmail.com>
2022-10-26 09:20:05 +0300

    On branch main  Changes to be committed: 	modified:   baseinstall.py

--- baseinstall.py
+++ baseinstall.py
@@ -3,76 +3,57 @@ from disks import DISKTOINSTALL
 
 class ArchInstall:
 
-    #ask what kernel the user prefers
+    #ask what kernel the user prefers and save it to a variable as str 
     def choose_kernel():
         print("Choose a kernel: ")
         print("1: linux")
         print("2: linux-lts")
-        print("3: linux-hardened")
-        print("4: linux-zen")
         kernel = input("Kernel: ")
         if kernel == "1":
-            kernel = "linux"
+            return " linux  linux-headers"
         elif kernel == "2":
-            kernel = "linux-lts"
-        elif kernel == "3":
-            kernel = "linux-hardened"
-        elif kernel == "4":
-            kernel = "linux-zen"
+            return " linux-lts linux-lts-headers"
         else:
             print("Invalid answer")
             ArchInstall.choose_kernel()
-        kernel_to_install = kernel
+        return kernel
         
-        
-
-    def headers():
-        kernel = ArchInstall.choose_kernel()
-        #based on kernel choose kernel headers
-        if kernel == "linux":
-            kernel_headers = "linux-headers"
-        elif kernel == "linux-lts":
-            kernel_headers = "linux-lts-headers"
-        elif kernel == "linux-hardened":
-            kernel_headers = "linux-hardened-headers"
-        elif kernel == "linux-zen":
-            kernel_headers = "linux-zen-headers"
-        kernel_headers_to_install = kernel_headers
-    
     #install the base system
     def base_system():
         kernel = ArchInstall.choose_kernel()
-        kernel_headers = ArchInstall.headers()
-
-        overall_to_install = kernel + " " + kernel_headers
+        #update pacman keys 
+        os.system('pacman -Sy --noconfirm archlinux-keyring')
+        #update pacman
+        os.system('pacman-keys --init')
+        #install base system
         print("Installing base system")
-        os.system('pacstrap', '/mnt', 'base' , overall_to_install , 'linux-firmware'  ,'base-devel','vim','networkmanager')
+        os.system('pacstrap /mnt base base-devel linux-firmware base-devel vim networkmanager' + kernel )
         #create fstab
-        os.system('genfstab', '-U', '/mnt', '>>', '/mnt/etc/fstab')
+        os.system('genfstab -U /mnt >> /mnt/etc/fstab')
         #chroot
-        os.system('arch-chroot', '/mnt')
+        os.system('arch-chroot /mnt')
         #set timezone based on user input
         print("Choose a timezone: ")
         os.system('ls', '/usr/share/zoneinfo')
         timezone = input("Timezone: ")
-        os.system('ln', '-sf', '/usr/share/zoneinfo/' + timezone, '/etc/localtime')
+        os.system('ln -sf /usr/share/zoneinfo/' + timezone + '/etc/localtime')
         #set hardware clock
-        os.system('hwclock', '--systohc')
+        os.system('hwclock --systohc')
         #set locale
         print("Choose a locale: ") 
-        os.system('ls', '/etc/locale.gen')
+        os.system('ls /etc/locale.gen')
         locale = input("Locale: ")
-        os.system('sed', '-i', "'s/#" + locale + "/" + locale + "/g'", '/etc/locale.gen')
+        os.system('sed -i' + "'s/#" + locale + "/" + locale + "/g'", '/etc/locale.gen')
         os.system('locale-gen')
-        os.system('echo', 'LANG=' + locale, '>', '/etc/locale.conf')
+        os.system('echo LANG=' + locale + '> /etc/locale.conf')
         #set hostname
         print("Enter hostname: ")
         hostname = input("Hostname: ")
-        os.system('echo', hostname, '>', '/etc/hostname')
+        os.system('echo' + hostname + '> /etc/hostname')
         #set basik hosts
-        os.system('echo', '127.0.0.1' + '\t' + 'localhost', '>', '/etc/hosts')
-        os.system('echo', '::1' + '\t' + 'localhost', '>>', '/etc/hosts')
-        os.system('echo', '127.0.1.1' + '\t' + hostname + '.localdomain' + '\t' + hostname, '>>', '/etc/hosts')
+        os.system('echo 127.0.0.1 \t localhost > /etc/hosts')
+        os.system('echo ::1 \t localhost >> /etc/hosts')
+        os.system('echo 127.0.1.1 \t' + hostname + '.localdomain \t' + hostname + '>> /etc/hosts')
         #set root password
         print("Enter root password: ")
         os.system('passwd')
@@ -93,25 +74,27 @@ class ArchInstall:
     #install grub
     def grub():
         disk = DISKTOINSTALL.choose_disk()
-        os.system('pacman', '-S', 'grub' , 'efibootmgr', 'os-prober', 'dosfstools', 'mtools')
-        os.system('mkdir', '/boot/EFI')
-        os.system('mount', disk + '1', '/boot/EFI')
-        os.system('grub-install', '--target=x86_64-efi', '--bootloader-id=GRUB', '--efi-directory=/boot/EFI')
-        os.system('grub-mkconfig', '-o', '/boot/grub/grub.cfg')
+        disk_name = disk[:-1]
+        os.system('pacman -S grub efibootmgr os-prober dosfstools mtools')
+        os.system('mkdir /boot/EFI')
+        os.system('mount' + disk_name + '1 /boot/EFI')
+        os.system('grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/EFI')
+        os.system('grub-mkconfig -o /boot/grub/grub.cfg')
         
     #install systemd-boot
     def systemd_boot():
         disk = DISKTOINSTALL.choose_disk()
-        os.system('bootctl', '--path=/boot', 'install')
-        os.system('echo', 'default arch', '>', '/boot/loader/loader.conf')
-        os.system('echo', 'timeout 4', '>>', '/boot/loader/loader.conf')
-        os.system('echo', 'editor 0', '>>', '/boot/loader/loader.conf')
-        os.system('echo', 'title Arch Linux', '>', '/boot/loader/entries/arch.conf')
-        os.system('echo', 'linux /vmlinuz-linux', '>>', '/boot/loader/entries/arch.conf')
-        os.system('echo', 'initrd /initramfs-linux.img', '>>', '/boot/loader/entries/arch.conf')
-        os.system('echo', 'options root=' + disk + '2 rw', '>>', '/boot/loader/entries/arch.conf')
+        disk_name = disk[:-1]
+        os.system('bootctl --path=/boot install')
+        os.system('echo default arch > /boot/loader/loader.conf')
+        os.system('echo timeout 4 >> /boot/loader/loader.conf')
+        os.system('echo editor 0 >> /boot/loader/loader.conf')
+        os.system('echo title Arch Linux > /boot/loader/entries/arch.conf')
+        os.system('echo linux /vmlinuz-linux >> /boot/loader/entries/arch.conf')
+        os.system('echo initrd /initramfs-linux.img >> /boot/loader/entries/arch.conf')
+        os.system('echo options root=' + disk_name + '2 rw >> /boot/loader/entries/arch.conf')
     
     #install networkmanager
     def networkmanager():
-        os.system('pacman', '-S', 'networkmanager')
-        os.system('systemctl', 'enable', 'NetworkManager')
+        os.system('pacman -S networkmanager')
+        os.system('systemctl enable NetworkManager')
